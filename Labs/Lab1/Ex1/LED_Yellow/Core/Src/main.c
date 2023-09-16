2fa3cde102e10097c96dea911be8bfb21af39704
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2023-09-16 12:11:44 +0700

    Edit ex1 lab1.

--- Labs/Lab1/Ex1/LED_Yellow/Core/Src/main.c
+++ Labs/Lab1/Ex1/LED_Yellow/Core/Src/main.c
@@ -108,12 +108,6 @@ int main(void)
 	  }
 	  counter++;
 	  HAL_Delay(1000);
-//	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, RESET);
-//	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, SET);
-//	HAL_Delay(2000);
-//	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, SET);
-//	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, RESET);
-//	HAL_Delay(2000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
